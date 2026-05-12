6a29a58bbe01af3e70b22616f5863e4bea8ba451
Jacob Norgard <jkn5395@psu.edu>
2026-05-12 15:53:14 -0400

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) 
+    {
+        // Integer array of nums of length n
+        int n = nums.size(); // Gets size off array int n.
+
+        // Need to make an arr with size 2n.
+        vector<int> ans(2 * n);
+
+
+        //Will need a for loop to iterate through array elements to copy to new array
+        for(int i = 0; i < n; i++)
+        {
+            
+            ans[i] = ans[i + n] = nums[i];
+        }
+        
+        return ans;
+    }
+};
